7a930a8982ba85234b656e784531b502d49de607
Thomas Demarty <48022911+thdemarty@users.noreply.github.com>
2025-04-08 14:16:25 +0200

    Auth views (#25)
    * Add a changelog.
    
    * Add instructions on how to contribute #18 (#19)
    
    * Contribute and Changelog
    
    * Add a changelog.
    * Add instructions on how to contribute #18 (#19)
    
    * Bump version (#20) (#24)
    
    * Add a changelog.
    
    * Add instructions on how to contribute #18 (#19)
    
    * Auth views (#21)
    
    * Add instructions on how to contribute #18
    
    * fix: change TEMPLATES DIRS, LANGUAGE_CODE and TIME_ZONE in settings
    
    * fix: wrong parameters name in .env.dist
    
    * feat: add login and logout views
    
    * feat: add login and logout template
    
    * feat: add User models to admin dashboard
    
    * feat: add register view
    
    * feat: add flag to disable registration

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.conf import settings
+from accounts.forms import RegisterForm
+
+
+def register(request):
+    if settings.ALLOW_REGISTRATION:
+        form = RegisterForm()
+        if request.method == "POST":
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect("login")
+        context = {"form": form}
+    else:
+        context = {"not_allowed": True}
+    return render(request, "register.html", context)
