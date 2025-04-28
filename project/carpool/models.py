f0d53caf878de8d76f6d1f286fb7c3ab6bb15ec5
mramel640 <maud.ramem@gmail.com>
2025-04-28 14:57:31 +0200

    feat(carpool) add several models * Ride #4 * Localisation #13 * Step #12

--- project/carpool/models.py
+++ project/carpool/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 
+class Localisation(models.Model):
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+
+class Step(models.Model):
+    name = models.TextField()
+    loc = models.ForeignKey(Localisation, on_delete=models.CASCADE)
+
+
 class Ride(models.Model):
     driver = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    rider = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     start_dt = models.DateTimeField()
     end_dt = models.DateTimeField()
+    start_loc = models.ForeignKey(Localisation, on_delete=models.CASCADE)
+    end_loc = models.ForeignKey(Localisation, on_delete=models.CASCADE)
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=20)
+    price = models.FloatField()
     comment = models.TextField()
 
 class Vehicle(models.Model):
