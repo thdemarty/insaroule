6e8443708c86bc7bcb33775e0a5ba33da0cb70be
Thomas Demarty <48022911+thdemarty@users.noreply.github.com>
2025-05-02 23:57:14 +0200

    Carpool models  (#39)
    * fix(ci) update workflow to run ci when pr on `develop` branch
    - Improve ride views (@mramel640 & @Yoshilde)
    - Add Step and Location models (@mramel640)
    - Modify Ride model (@mramel640)
    - Register new models in admin panel (@thdemarty)
    - Add validators to new models (@thdemarty)
    - Add verbose_name to each field of all models (@thdemarty)
    - Add help_text to each field of each models (@thdemarty)
    - Add blank and null to some fields (@thdemarty)
    - Update the migration (@thdemarty)
    - Change navigation bar in list rides view (@thdemarty)
    ---------
    Co-authored-by: Mathilde Sergent <msergent@insa-rennes.fr>
    Co-authored-by: Maud Ramel <maud.ramel@insa-rennes.fr>
    Co-authored-by: thdemarty <48022911+thdemarty@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: mramel640 <maud.ramem@gmail.com>

--- project/carpool/views.py
+++ project/carpool/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from carpool.models import Ride
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def rides_list(request):
     context = {
         "rides": Ride.objects.all(),
