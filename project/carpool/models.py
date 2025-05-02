a465e9a8623a876a1b68d8c1b5531c87086f45e8
mramel640 <maud.ramem@gmail.com>
2025-05-02 11:45:13 +0200

    feat(carpool) : Ride list view fixes #5

--- project/carpool/models.py
+++ project/carpool/models.py
@@ -12,16 +12,25 @@ class Step(models.Model):
 
 
 class Ride(models.Model):
-    driver = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-    rider = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    driver = models.ForeignKey(
+        "accounts.User", on_delete=models.CASCADE, related_name="rides_as_driver"
+    )
+    rider = models.ForeignKey(
+        "accounts.User", on_delete=models.CASCADE, related_name="rides_as_rider"
+    )
     start_dt = models.DateTimeField()
     end_dt = models.DateTimeField()
-    start_loc = models.ForeignKey(Localisation, on_delete=models.CASCADE)
-    end_loc = models.ForeignKey(Localisation, on_delete=models.CASCADE)
+    start_loc = models.ForeignKey(
+        Localisation, on_delete=models.CASCADE, related_name="rides_start_here"
+    )
+    end_loc = models.ForeignKey(
+        Localisation, on_delete=models.CASCADE, related_name="rides_ends_here"
+    )
     step = models.ForeignKey(Step, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=20)
     price = models.FloatField()
     comment = models.TextField()
+
 
 class Vehicle(models.Model):
     name = models.CharField(max_length=50)
