115a18c30a2e85233d55775c4903d9d5cfa8665d
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-07-11 14:34:15 +0200

    feat(icons) add favicons and serving favicon in debug mode - Added support to site.webmanifest to act as a PWA

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,55 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
     path("", include("carpool.urls", namespace="carpool")),
 ]
+
+if settings.DEBUG:
+    from django.views.static import serve
+
+    urlpatterns += [
+        path(
+            "favicon.ico",
+            serve,
+            {
+                "path": "favicon.ico",
+                "document_root": settings.STATIC_ROOT,
+            },
+        ),
+        path(
+            "site.webmanifest",
+            serve,
+            {
+                "path": "site.webmanifest",
+                "document_root": settings.STATIC_ROOT,
+            },
+        ),
+        path(
+            "apple-touch-icon.png",
+            serve,
+            {
+                "path": "apple-touch-icon.png",
+                "document_root": settings.STATIC_ROOT,
+            },
+        ),
+        path(
+            "favicon-32x32.png",
+            serve,
+            {
+                "path": "favicon-32x32.png",
+                "document_root": settings.STATIC_ROOT,
+            },
+        ),
+        path(
+            "favicon-16x16.png",
+            serve,
+            {
+                "path": "favicon-16x16.png",
+                "document_root": settings.STATIC_ROOT,
+            },
+        ),
+    ]
