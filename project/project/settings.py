677fd133ee4fff246000c82fc2de8b9a12097edf
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-07-11 15:24:18 +0200

    fix(test) fix testing configuration

--- project/project/settings.py
+++ project/project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
