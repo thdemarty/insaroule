5901c0924030d3de40e9c2256c992c2a2b756744
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-08-29 11:15:57 +0200

    Notification preference editor in account center

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from accounts.models import User
+from accounts.models import User, UserNotificationPreferences
 
 
 @admin.register(User)
@@ -16,3 +16,8 @@ class UserAdmin(BaseUserAdmin):
             {"fields": ("email_verified", "last_verification_email_sent")},
         ),
     )
+
+
+@admin.register(UserNotificationPreferences)
+class UserNotificationPreferencesAdmin(admin.ModelAdmin):
+    pass
---
+++ project/accounts/signals.py
@@ -0,0 +1,12 @@
+# Create a UserNotificationPreferences every time a User is created
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+from accounts.models import User, UserNotificationPreferences
+
+
+@receiver(post_save, sender=User)
+def create_user_notification_preferences(sender, instance, created, **kwargs):
+    if created:
+        UserNotificationPreferences.objects.create(user=instance)
