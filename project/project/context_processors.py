d0eb762914f6cbcfa03f3f3067c39d52b22bc744
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-09-05 20:48:32 +0200

    Display rides to anonymous users

--- project/project/context_processors.py
+++ project/project/context_processors.py
@@ -4,4 +4,6 @@ from django.conf import settings
 def constants(request):
     return {
         "SUPPORT_EMAIL": settings.SUPPORT_EMAIL,
+        "TERMS_OF_SERVICE": settings.TERMS_OF_SERVICE,
+        "PRIVACY_POLICY": settings.PRIVACY_POLICY,
     }
