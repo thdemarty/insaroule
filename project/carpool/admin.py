2b9fa2ed228aa46c630a50e30dde3b0da6cab060
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-09-12 17:54:49 +0200

    Admin panel improvement for Vehicle model

--- project/carpool/admin.py
+++ project/carpool/admin.py
@@ -4,11 +4,15 @@ from carpool.models import Location, Step, Vehicle
 from carpool.models.ride import Ride
 from carpool.models.statistics import Statistics, MonthlyStatistics
 
-admin.site.register(Vehicle)
 admin.site.register(Location)
 admin.site.register(Step)
 
 admin.site.register(Statistics)
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ("name", "driver", "seats", "description", "geqCO2_per_km")
 
 
 @admin.register(MonthlyStatistics)
