83a6ddc5b751f03ea156e038f465ca0e89b3b02f
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-09-26 21:39:32 +0200

    test: anonymous_access_rides_list test added

---
+++ project/carpool/tests/test_views.py
@@ -0,0 +1,28 @@
+from accounts.tests.factories import UserFactory
+
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse
+
+from carpool.tests.factories import RideFactory
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        u1 = UserFactory()
+        self.r1 = RideFactory(driver=u1)
+        self.r2 = RideFactory(driver=u1)
+
+    def test_anonymous_access_rides_list(self):
+        # Anonymous access to rides list is disabled
+        settings.ANONYMOUS_ACCESS_RIDES_LIST = False
+        r = self.client.get(reverse("carpool:list"))
+        self.assertEqual(r.status_code, 302)
+        self.assertIn(reverse("accounts:login"), r.url)
+
+        # Anonymous access to rides list is enabled
+        settings.ANONYMOUS_ACCESS_RIDES_LIST = True
+        r = self.client.get(reverse("carpool:list"))
+        self.assertEqual(r.status_code, 200)
+        self.assertIn(self.r1, r.context["rides"])
+        self.assertIn(self.r2, r.context["rides"])
