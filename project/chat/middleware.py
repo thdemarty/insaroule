7ad64848cbb6ed134cf96e9a36a50ccdd28b760e
thdemarty <48022911+thdemarty@users.noreply.github.com>
2026-04-01 11:39:04 +0200

    Fix middleware ?

--- project/chat/middleware.py
+++ project/chat/middleware.py
@@ -1,39 +1,33 @@
+# chat/middleware.py
 from channels.db import database_sync_to_async
 
 
 @database_sync_to_async
-def get_user(token_key):
-    from django.contrib.auth.models import AnonymousUser
-    from django.contrib.auth import get_user_model
-    from rest_framework_simplejwt.tokens import AccessToken
-
+def get_user_from_jwt(token_key):
     try:
+        from django.contrib.auth import get_user_model
+        from rest_framework_simplejwt.tokens import AccessToken
+
+        User = get_user_model()
         access_token = AccessToken(token_key)
-        return get_user_model().objects.get(id=access_token["user_id"])
-    except Exception:
-        return AnonymousUser()
+        return User.objects.get(pk=access_token["user_id"])
+
+    except Exception as e:
+        print("JWTAuthMiddleware error:", e)
+        return None
 
 
 class JWTAuthMiddleware:
-    """
-    Custom middleware that takes a JWT token from the query string and authenticates the user.
-    """
-
     def __init__(self, inner):
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        from django.contrib.auth.models import AnonymousUser
+        headers = dict(scope.get("headers", {}))
+        auth_header = headers.get(b"authorization", b"").decode().split()
 
-        headers = dict(scope["headers"])
-        if b"authorization" in headers:
-            try:
-                token_name, token_key = headers[b"authorization"].decode().split()
-                if token_name.lower() == "bearer":
-                    scope["user"] = await get_user(token_key)
-            except Exception:
-                scope["user"] = AnonymousUser()
-        else:
-            scope["user"] = AnonymousUser()
+        if len(auth_header) == 2 and auth_header[0].lower() == "bearer":
+            jwt_user = await get_user_from_jwt(auth_header[1])
 
+            if jwt_user:
+                scope["user"] = jwt_user
         return await self.inner(scope, receive, send)
